8dbc99dbdafb697ba5770c71600e2992abd26e52
Sergey Tsvetkov <tsvetkov71@gmail.com>
2024-03-04 18:14:40 +0300

    Lab13

---
+++ Lab13_control_1(polymorphism)/human.h
@@ -0,0 +1,33 @@
+#include <string>
+#include <sstream>
+#pragma once /* Защита от двойного подключения заголовочного файла */
+using namespace std;
+
+class human {
+public:
+	// Конструктор класса human
+	human(string last_name, string name, string second_name)
+	{
+		this->last_name = last_name;
+		this->name = name;
+		this->second_name = second_name;
+	}
+	// Получение ФИО человека
+	string get_full_name()
+	{
+		ostringstream full_name;
+		full_name << this->last_name << " "
+			<< this->name << " "
+			<< this->second_name;
+		return full_name.str();
+	}
+
+	virtual string get_info() {
+		return "ФИО: " + get_full_name();
+	}
+
+private:
+	string name; // имя
+	string last_name; // фамилия
+	string second_name; // отчество
+};
